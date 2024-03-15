f5b61fbbe15d6215586feeb57aaca978b02f58b6
Sobirobert <rsobierajpsp@gmail.com>
2024-03-15 11:29:41 +0100

    5.6 Rejestrowanie użytkownika

--- Blogger/Infrastructure/Data/BloggerContext.cs
+++ Blogger/Infrastructure/Data/BloggerContext.cs
@@ -1,13 +1,15 @@
 ﻿
 using Domain.Common;
 using Domain.Entities;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 
-public class BloggerContext : DbContext
+public class BloggerContext : IdentityDbContext<ApplicationUser>
 {
-    public BloggerContext(DbContextOptions options) : base(options)
+    public BloggerContext(DbContextOptions<BloggerContext> options) : base(options)
     {
 
     }
--- Blogger/WebAPI/Wrappers/Response.cs
+++ Blogger/WebAPI/Wrappers/Response.cs
@@ -4,6 +4,8 @@ public class Response<T>
 {
     public T Data { get; set; }
     public bool Succeeded { get; set; }
+    public string Message { get; set; }
+    public IEnumerable<string> Errors { get; set; }
     public Response()
     {
 
